2bf5ab9ebb7a2d8ec0543ed56debc7578232bf86
oleksiitymchenko <36442865+oleksiitymchenko@users.noreply.github.com>
2018-07-28 01:27:01 +0300

    flights detail card

--- AirportFrontend/src/app/flights/flights-detail/flights-detail.component.ts
+++ AirportFrontend/src/app/flights/flights-detail/flights-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FlightsService } from '../flights.service';
 import { ActivatedRoute } from '@angular/router';
 import FlightDTO from '../../shared/flightDto';
+import TicketDto from 'src/app/shared/ticketDto';
 
 @Component({
   selector: 'app-flights-detail',
@@ -10,15 +11,25 @@ import FlightDTO from '../../shared/flightDto';
   providers: [FlightsService]
 })
 export class FlightsDetailComponent implements OnInit {
-  flight:FlightDTO;
+    flight:FlightDTO;
     id:number;
+    tickets: Array<TicketDto>;
+    ticketids:number[];
+    ticketid : number;
+    allowchange:boolean;
 
   constructor(private service: FlightsService, private route: ActivatedRoute) { 
-  }
-
-  ngOnInit() {
     this.route.params.subscribe(params=>this.id=params['id'])
     this.service.getOneFlight(this.id)
     .subscribe((data:FlightDTO)=>{this.flight=data;console.log(this.flight)});
+    this.allowchange=false;
+  }
+
+  onUpdateClick()
+  {
+    this.allowchange=true;
+  }
+  ngOnInit() {
+    
   }
 }
