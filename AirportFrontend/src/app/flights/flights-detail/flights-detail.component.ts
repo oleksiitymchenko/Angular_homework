d3cf7cb73db3af69bee928a76bd661a9ee66eabc
oleksiitymchenko <36442865+oleksiitymchenko@users.noreply.github.com>
2018-07-28 01:47:52 +0300

    flights

--- AirportFrontend/src/app/flights/flights-detail/flights-detail.component.ts
+++ AirportFrontend/src/app/flights/flights-detail/flights-detail.component.ts
@@ -3,6 +3,7 @@ import { FlightsService } from '../flights.service';
 import { ActivatedRoute } from '@angular/router';
 import FlightDTO from '../../shared/flightDto';
 import TicketDto from 'src/app/shared/ticketDto';
+import { TicketsService } from 'src/app/tickets/tickets.service';
 
 @Component({
   selector: 'app-flights-detail',
@@ -18,17 +19,33 @@ export class FlightsDetailComponent implements OnInit {
     ticketid : number;
     allowchange:boolean;
 
-  constructor(private service: FlightsService, private route: ActivatedRoute) { 
+  constructor(private service: FlightsService, private tserv:TicketsService,private route: ActivatedRoute) { 
     this.route.params.subscribe(params=>this.id=params['id'])
     this.service.getOneFlight(this.id)
-    .subscribe((data:FlightDTO)=>{this.flight=data;console.log(this.flight)});
+    .subscribe((data:FlightDTO)=>{this.flight=data;
+                                  console.log(this.flight);
+                                  this.tserv.getTickets().subscribe(
+                                        (data:Array<TicketDto>)=>this.tickets=data);});
     this.allowchange=false;
+    this.ticketids=[];
   }
+  AddTicket(id: number,ticketids:number[])
+  {
 
+    ticketids.push(id);
+    const number = this.tickets.findIndex(item => (item['id'] == id));
+    this.tickets.splice(number, 1);
+ }
   onUpdateClick()
   {
     this.allowchange=true;
   }
+  flightSaveUpdates(id: number,flight:FlightDTO) {
+    flight.ticketIds=this.ticketids;
+    this.service.updateFlight(id, flight).subscribe((res:Response)=>console.log(res));
+    
+
+   }
   ngOnInit() {
     
   }
