065da5f7a3fac102ce0d4c1803902b0ec0910887
oleksiitymchenko <36442865+oleksiitymchenko@users.noreply.github.com>
2018-07-27 18:07:13 +0300

    stewardess detail

--- AirportFrontend/src/app/stewardess/stewardess-detail/stewardess-detail.component.ts
+++ AirportFrontend/src/app/stewardess/stewardess-detail/stewardess-detail.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { StewardessService } from '../stewardess.service';
-import PilotDto from '../../shared/pilotDto';
 import { ActivatedRoute } from '@angular/router';
 import StewardessDto from '../../shared/stewardessDto';
 
@@ -11,15 +10,30 @@ import StewardessDto from '../../shared/stewardessDto';
   providers: [StewardessService]
 })
 export class StewardessDetailComponent implements OnInit {
-  stewardess:StewardessDto;
+    
+    stewardess:StewardessDto;
     id:number;
+    allowchange:boolean;
 
-  constructor(private service: StewardessService, private route: ActivatedRoute) { 
+  constructor(private service: StewardessService, private route: ActivatedRoute) {  
+    this.stewardess = new StewardessDto(undefined,undefined,undefined,undefined);
+    this.allowchange = false;
+    this.route.params.subscribe(params=>this.id=params['id']);
+    this.service.getOneStewardess(this.id)
+        .subscribe((data:StewardessDto)=>{this.stewardess=data;console.log(this.stewardess)});
+    console.log(this.stewardess);
   }
+
+  stewardessSaveUpdates(id: number,stewardess) {
+    console.log(stewardess);
+    this.service.updateStewardess(id, stewardess).subscribe((res:Response)=>console.log(res));
+  }
+  onUpdateClick()
+  {
+    this.allowchange = true;
+  }
+
 
   ngOnInit() {
-    this.route.params.subscribe(params=>this.id=params['id'])
-    this.service.getOneStewardess(this.id)
-    .subscribe((data:StewardessDto)=>{this.stewardess=data;console.log(this.stewardess)});
-  }
+    }
 }
